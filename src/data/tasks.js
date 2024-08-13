e54e72d1bcb765d39b8451fd1d9fe7ef13c2ed33
GiedreKite <giedre.narvilaite@gmail.com>
2024-08-13 18:15:25 +0300

    up

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,23 +1,44 @@
 export const tasks = [
     {
+        id: 1,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia issivalyti dantis',
     },
     {
+        id: 2,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia nusiprausti veidi',
     },
     {
+        id: 3,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia pasigaminti gerima',
     },
     {
+        id: 4,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia sestis/stotis prie PC',
     },
     {
+        id: 5,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia sukodinti krepsinio lentos uzduoti',
     },
     {
+        id: 6,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Reikia isspresti su kolegomis klausima del, "Kaip paviesinti reactini projekta per Github Pages?"',
     },
     {
+        id: 7,
+        color: '#cccccc',
+        state: 'todo',
         text: 'Gerai issimiegoti... 😴😴😴',
     },
 ];
